28de1156f650451a0a971e50b356275e0dbde46b
avykun1 <30806028+avykun1@users.noreply.github.com>
2022-09-19 15:31:01 +0530

    Update App.java

--- src/main/java/Sample/test_sample/App.java
+++ src/main/java/Sample/test_sample/App.java
@@ -9,7 +9,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		try {
-			test.login_MFA("CHROME");
+			test.login_MFA("IE");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			System.out.println("Check the credentials and Re-execute");
