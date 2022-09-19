a9567e670bfeb8f48a2e33cc1f0a8a15aaade5f2
avykun1 <30806028+avykun1@users.noreply.github.com>
2022-09-19 15:36:36 +0530

    reverted

--- src/main/java/Sample/test_sample/App.java
+++ src/main/java/Sample/test_sample/App.java
@@ -9,7 +9,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		try {
-			test.login_MFA("IE");
+			test.login_MFA("EDGE");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			System.out.println("Check the credentials and Re-execute");
