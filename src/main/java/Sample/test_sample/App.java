ce60fe7397dc53918a25a19006517766996e5154
avykuntham <anil_k_vykuntham@optum.com>
2022-09-19 15:22:47 +0530

    sample commit

--- src/main/java/Sample/test_sample/App.java
+++ src/main/java/Sample/test_sample/App.java
@@ -9,7 +9,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		try {
-			test.login_MFA("EDGE");
+			test.login_MFA("CHROME");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			System.out.println("Check the credentials and Re-execute");
